58ba5eb6f78cd402f0cb293f41cb231f82e701cc
guts <krsproutbro@gmail.com>
2022-12-30 16:36:52 +0900

    테스트 페이지 만듬

---
+++ src/main/java/cf/eisp/spring_jsp_pds/controller/TestController.java
@@ -0,0 +1,12 @@
+package cf.eisp.spring_jsp_pds.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<h1>테스트 페잊</h1>
